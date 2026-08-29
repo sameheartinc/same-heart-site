c645dd74c45ee51edc46cd31adc7dd2e7d83f2a7
Same Heart Inc <sameheartinc@gmail.com>
2026-08-29 11:23:32 -0400

    verifying

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Same Heart",
   description: "SAMEHEART™ — First Signal",
+  verification: {
+    google: "oGB17x90qYbnnnYrJRvDIrcVjveGqy74DeYKxMh_s64",
+  },
 };
 
 export default function RootLayout({
